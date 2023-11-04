df5f3009b7307dc85805d0229baf4983f4da905c
Ruona125 <ogheneruonaagadagba4@gmail.com>
2023-11-04 11:40:23 +0100

    created the wishlist

---
+++ src/pages/Wishlists/Wishlists.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import WishlistComponent from '../../components/WishlistsComponent/WishlistComponent'
+
+const Wishlists = () => {
+  return (
+    <div>
+        <WishlistComponent />
+    </div>
+  )
+}
+
+export default Wishlists
