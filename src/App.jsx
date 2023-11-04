81526c3efc5d17f5655ad1915e0e2d4003d9a72a
Ruona125 <ogheneruonaagadagba4@gmail.com>
2023-11-04 12:03:26 +0100

    added some pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import {Routes, Route} from "react-router-dom"
 import Login from "./pages/Login/Login"
 import HomePage from "./pages/Home/HomePage"
+import ModifyOrder from "./pages/ModifyOrder/ModifyOrder"
+import ViewOrder from "./pages/ViewOrder/ViewOrder"
+import Wishlists from "./pages/Wishlists/Wishlists"
+import ViewProductPage from "./pages/ViewProduct/ViewProductPage"
 
 function App() {
   return (
@@ -9,6 +13,12 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/home" element={<HomePage />} />
+          <Route path="/modify-order/:id" element={<ModifyOrder />} />
+          <Route path="/orders" element={<ViewOrder />} />
+          <Route path="/wishlists" element={<Wishlists />} />
+          <Route path="/products" element={<ViewProductPage />} />
+          <Route path="/products/:id" element={<ViewProductPage />} />
+
         </Routes>
       </div>
     </>
