8c242ac44fbb29f9e1b86854782056cd9ac3e083
Ruona125 <ogheneruonaagadagba4@gmail.com>
2023-11-04 13:12:44 +0100

    createdh the create product form, yet to test it

--- src/pages/CreateProduct/CreateProduct.jsx
+++ src/pages/CreateProduct/CreateProduct.jsx
@@ -1,9 +1,184 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import CircularProgress from "@mui/material/CircularProgress";
+import { useSelector } from "react-redux";
+import { Button } from "@mui/material";
 
 const CreateProduct = () => {
-  return (
-    <div>CreateProduct</div>
-  )
-}
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [category, setCategory] = useState("");
+  const [description, setDescription] = useState("");
+  const [image, setImage] = useState("");
+  const [reviews, setReviews] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
 
-export default CreateProduct
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const url = "hhttp://localhost:8000/product";
+    const data = {
+      name: name,
+      category: category,
+      price: price,
+      description: description,
+      image: image,
+      reviews: reviews,
+    };
+    const headers = {
+      "Content-Type": "application/json",
+      //   Authorization: token,
+    };
+    setLoading(true);
+
+    try {
+      const res = await axios.post(url, data, { headers });
+      if (res.status === 200) {
+        setName("");
+        setCategory("");
+        setPrice("");
+        setDescription("");
+        setImage("");
+        setReviews("");
+        setLoading(false);
+        navigate("/products");
+      } else {
+        console.log("error sending data");
+        setError("An error occurred, please try again");
+      }
+    } catch (err) {
+      console.log(err);
+      setLoading(false);
+      setError("An error occurred, please try again");
+    }
+  };
+  return (
+    <div>
+      <h2>Create Product</h2>
+      <Box
+        sx={{
+          "& > :not(style)": {
+            m: 1,
+            width: "25ch",
+          },
+          "& .MuiOutlinedInput-root": {
+            "& fieldset": {
+              borderColor: "red",
+            },
+            "&:hover fieldset": {
+              borderColor: "red",
+            },
+            "&.Mui-focused fieldset": {
+              borderColor: "red",
+            },
+          },
+          "& .MuiInputLabel-root": {
+            color: "red",
+          },
+        }}
+        // style={{ display: "grid", marginTop: "23px" }}
+        // noValidate
+        // autoComplete="off"
+        // onSubmit={handleSubmit}
+      >
+        <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+          <TextField
+            id="outlined-basic"
+            label="product name"
+            variant="outlined"
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            InputLabelProps={{ style: { color: "red" } }}
+          />
+          <br />
+          <br />
+
+          <TextField
+            id="outlined-basic"
+            label="price"
+            variant="outlined"
+            type="number"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+            InputLabelProps={{ style: { color: "red" } }}
+          />
+          <br />
+          <br />
+
+          <TextField
+            id="outlined-basic"
+            label="category"
+            variant="outlined"
+            type="text"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+            InputLabelProps={{ style: { color: "red" } }}
+          />
+          <br />
+          <br />
+
+          <TextField
+            id="outlined-basic"
+            label="description"
+            variant="outlined"
+            type="text"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            InputLabelProps={{ style: { color: "red" } }}
+          />
+
+          <br />
+          <br />
+
+          <TextField
+            id="outlined-basic"
+            label="image"
+            variant="outlined"
+            type="text"
+            value={image}
+            onChange={(e) => setImage(e.target.value)}
+            InputLabelProps={{ style: { color: "red" } }}
+          />
+
+          <TextField
+            id="outlined-basic"
+            label="description"
+            variant="outlined"
+            type="text"
+            value={reviews}
+            onChange={(e) => setReviews(e.target.value)}
+            InputLabelProps={{ style: { color: "red" } }}
+          />
+
+          <center>
+            {loading ? (
+              <CircularProgress style={{ color: "red" }} />
+            ) : (
+              <Button
+                type="submit"
+                variant="contained"
+                className="login-button"
+                style={{
+                  color: "#fff",
+                  backgroundColor: "red",
+                  marginTop: "23px",
+                }}
+              >
+                Create Product
+              </Button>
+            )}
+            {error && <p style={{ color: "red" }}>{error}</p>}
+          </center>
+        </form>
+      </Box>
+    </div>
+  );
+};
+
+export default CreateProduct;
