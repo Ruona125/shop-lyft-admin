777cf09e87bdadea4ff7e3ad03a23ba568023a59
Ruona125 <ogheneruonaagadagba4@gmail.com>
2023-11-04 11:38:51 +0100

    started the page to view product

---
+++ src/components/Products/ViewProductComponent/ViewProductComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ViewProductComponent = () => {
+  return (
+    <div>ViewProductComponent</div>
+  )
+}
+
+export default ViewProductComponent
---
+++ src/pages/ViewProduct/ViewProductPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ViewProductComponent from '../../components/Products/ViewProductComponent/ViewProductComponent'
+
+const ViewProductPage = () => {
+  return (
+    <div>
+        <ViewProductComponent />
+    </div>
+  )
+}
+
+export default ViewProductPage
