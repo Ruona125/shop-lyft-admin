2cdffe42b3a82311ccd70ed52fca43fddd64565c
Ruona125 <ogheneruonaagadagba4@gmail.com>
2023-11-06 10:05:19 +0100

    did a rough design on the view product

--- src/components/Products/ViewProductComponent/ViewProductComponent.jsx
+++ src/components/Products/ViewProductComponent/ViewProductComponent.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import "./product-style.css";
 
 const ViewProductComponent = () => {
   const [products, setProducts] = useState([]);
@@ -13,39 +14,27 @@ const ViewProductComponent = () => {
     });
   }, []);
 
-  //   const handleDelete = (productId) => {
-  //     const url = `http://localhost:8000/product/${productId}`;
-
-  //     axios
-  //       .delete(url)
-  //       .then(() => {
-  //         // Remove the deleted product from the state
-  //         setProducts((prevProducts) => prevProducts.filter((product) => product._id !== productId));
-  //       })
-  //       .catch((error) => {
-  //         console.error("Error deleting product: " + error);
-  //       });
-  //   };
-
   return (
     <div style={{ textAlign: "center" }}>
       {/* <p>{user.email}</p> */}
       <h3>Products</h3>
-      {products.map((product) => (
-        <div key={product._id}>
-          <Link to={`/product/${product._id}`}>
-            <img width="200px" src={product.image} alt="hair" />
-            <p>Name: {product.name}</p>
-            <p>${product.price}</p>
-            <p>ratings: {product.ratings}</p>
-            {/* <button onClick={() => handleDelete(product._id)}>Delete</button> */}
-          </Link>
-          <Link to={`/modify-product/${product._id}`}>
-            <button>Modify</button>
-          </Link>
-          <br />
-        </div>
-      ))}
+      <div className="product-container">
+        {products.map((product) => (
+          <div key={product._id}>
+            <Link to={`/product/${product._id}`}>
+              <img width="200px" src={product.image} alt="hair" />
+              <p>Name: {product.name}</p>
+              <p>${product.price}</p>
+              <p>ratings: {product.ratings}</p>
+              {/* <button onClick={() => handleDelete(product._id)}>Delete</button> */}
+            </Link>
+            <Link to={`/modify-product/${product._id}`}>
+              <button>Modify</button>
+            </Link>
+            <br />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
