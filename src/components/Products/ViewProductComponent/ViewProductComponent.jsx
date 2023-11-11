9b213c72cec18be3a7027e84fcbfe388bf5fa2f4
Ruona125 <ogheneruonaagadagba4@gmail.com>
2023-11-11 23:42:50 +0100

    worked on the product page

--- src/components/Products/ViewProductComponent/ViewProductComponent.jsx
+++ src/components/Products/ViewProductComponent/ViewProductComponent.jsx
@@ -10,35 +10,53 @@ const ViewProductComponent = () => {
     const url = "http://localhost:8000/product";
     axios.get(url).then((response) => {
       setProducts(response.data);
-      //   console.log(response.data);
     });
   }, []);
 
   return (
-    <div style={{ textAlign: "center" }}>
-      {/* <p>{user.email}</p> */}
-      <h2>Products</h2>
-      <br />
-      <div className="product-container">
-        {products.map((product) => (
-          <div key={product._id}>
-            <Link to={`/product/${product._id}`} style={{textDecoration:"none"}}>
-              <div className="img-wrapper">
-              <img width="200px" src={product.imageLink} alt="hair" />
-              </div>
-              <p style={{fontFamily:'Edu TAS Beginner, cursive', color:"grey"}}>{product.category}</p>
-              <p style={{fontFamily:'Edu TAS Beginner, cursive'}}>{product.name}</p>
-              <p style={{fontFamily:'Edu TAS Beginner, cursive'}}>{product.price}</p>
-              {/* <button onClick={() => handleDelete(product._id)}>Delete</button> */}
-            </Link>
-            <Link to={`/modify-product/${product._id}`} style={{textDecoration:"none", color:"#000"}}>
-              <p className="func-button">Modify</p>
-            </Link>
-            <br />
-          </div>
-        ))}
+    <>
+      <div style={{ textAlign: "center" }}>
+        <h2>Products</h2>
+        <br />
+        <div className="product-container">
+          {products.map((product) => (
+            <div key={product._id} className="product-item">
+              <Link
+                to={`/product/${product._id}`}
+                style={{ textDecoration: "none" }}
+              >
+                <div className="image-wrapper">
+                  <img width="200px" src={product.imageLink} alt="hair" />
+                </div>
+                <div className="details-wrapper">
+                  <p
+                    style={{
+                      fontFamily: "Edu TAS Beginner, cursive",
+                      color: "grey",
+                    }}
+                  >
+                    {product.category}
+                  </p>
+                  <p style={{ fontFamily: "Edu TAS Beginner, cursive" }}>
+                    {product.name}
+                  </p>
+                  <p style={{ fontFamily: "Edu TAS Beginner, cursive" }}>
+                    ${product.price}
+                  </p>
+                  <Link
+                    to={`/modify-product/${product._id}`}
+                    style={{ textDecoration: "none", color: "#000" }}
+                  >
+                    <p className="func-button">Modify</p>
+                    <br />
+                  </Link>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
