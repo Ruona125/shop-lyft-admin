3c8f334fbc58d680980d9b3c9c6c112d6a621d52
ruona <ogheneruonaagadagba4@gmail.com>
2023-11-15 15:47:33 +0100

    worked on the part to view certain product

--- src/components/Products/ViewCertainProduct/ViewCertainProductComponent.jsx
+++ src/components/Products/ViewCertainProduct/ViewCertainProductComponent.jsx
@@ -1,24 +1,58 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const ViewCertainProductComponent = () => {
   const { id } = useParams();
-  const [certainProducts, setCertainProduct] = useState([]);
+  const [certainProducts, setCertainProduct] = useState({ imageLinks: [] });
+
   useEffect(() => {
-    const url = `http://localhost:8000/product/${id}`;
-    axios.get(url).then((response) => {
-      setCertainProduct(response.data);
-      console.log(response.data);
-    });
-  }, []);
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`http://localhost:8000/product/${id}`);
+        setCertainProduct(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData(); // Call the async function inside useEffect
+  }, [id]); // Include 'id' in the dependency array
+
   return (
     <div>
       <h3>Certain Product</h3>
-      <img width="200px" src={certainProducts.imageLink} alt="hair" />
+
+      <div className="details-wrapper">
+        <Carousel
+          showArrows={true}
+          showStatus={false}
+          showThumbs={false}
+          dynamicHeight={false}
+          emulateTouch={true}
+          infiniteLoop={true}
+          autoPlay={true}
+          interval={2000}
+        >
+          {certainProducts.imageLinks?.map((link, index) => (
+            <div key={index}>
+              <img
+                width="70px"
+                height="200px"
+                src={link}
+                alt={`hair-${index}`}
+              />
+            </div>
+          ))}
+        </Carousel>
+      </div>
+
       <p>Name: {certainProducts.name}</p>
       <p>$ {certainProducts.price}</p>
-      <p>ratings: {certainProducts.ratings}</p>
+      <p>Ratings: {certainProducts.ratings}</p>
     </div>
   );
 };
