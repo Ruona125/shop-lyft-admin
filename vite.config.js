95d3fe2d9f520f208b7b8c58129bc5929ef1a0a9
Ruona125 <ogheneruonaagadagba4@gmail.com>
2023-11-22 16:35:25 +0100

    finally wrapped up everything

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   // server: {
   //   proxy: {
-  //     '/api':'https://api.bucollections.com'
+  //     '/api':'https://api.bucollections.com/'
   //   }
   // }
 })
