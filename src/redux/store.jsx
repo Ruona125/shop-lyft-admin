9282672ef49f8f16b1dc99490d522a5a2986784a
Ruona125 <ogheneruonaagadagba4@gmail.com>
2024-03-16 08:36:45 +0100

    modified the store.jsx

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,31 +1,57 @@
-// store.js
+// // store.js
+
+// import { configureStore } from '@reduxjs/toolkit';
+// import authReducer from './authSlice';
+// import { persistStore, persistReducer } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
+// import cartReducer from "./cartSlice"
+// // const middleware = [
+// //     ...getDefaultMiddleware(),// Add your middleware
+// //   ];
+
+// const persistConfig = {
+//   key: 'root',
+//   storage,
+//   // blacklist: ['auth'],
+// };
+
+// const persistedReducer = persistReducer(persistConfig, authReducer, cartReducer);
+
+// export const store = configureStore({
+//   reducer: {
+//     auth: persistedReducer,
+//     cart: cartReducer
+//   },
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware({
+//       serializableCheck: false
+//     }),
+// });
+
+// export const persistor = persistStore(store);
+
+// store.jsx
 
 import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './authSlice';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-import cartReducer from "./cartSlice"
-// const middleware = [
-//     ...getDefaultMiddleware(),// Add your middleware
-//   ];
+import authReducer from './authSlice';
+import cartReducer from './cartSlice';
 
 const persistConfig = {
   key: 'root',
   storage,
-  // blacklist: ['auth'],
 };
 
-const persistedReducer = persistReducer(persistConfig, authReducer, cartReducer);
+const rootReducer = {
+  auth: authReducer,
+  cart: cartReducer
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    auth: persistedReducer,
-    cart: cartReducer
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: false
-    }),
+  reducer: persistedReducer,
 });
 
 export const persistor = persistStore(store);
